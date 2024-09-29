c02ec8109d7fc2aee8a0db8a1d3d0414fb353037
codystack <ebuzzadvert@gmail.com>
2024-09-29 14:40:01 +0100

    about page added

---
+++ about.php
@@ -0,0 +1,79 @@
+<?php
+include "./components/header.php";
+include "./components/navbar.php";
+?>
+
+		<div class="inner-banner-one pt-225 lg-pt-200 md-pt-150 pb-100 md-pb-70 position-relative" style="background-image: url(images/about-bg.webp);background-position: center center;">
+			<div class="container position-relative">
+				<div class="row align-items-center">
+					<div class="col-lg-7">
+						<h1 class="hero-heading d-inline-block position-relative text-dark me-xl-4">We’ve got you covered. <img src="images/shape/shape_25.svg" alt=""></h1>
+					</div>
+				</div>
+			</div>
+		</div>
+
+        <div class="text-feature-three text-feature-one position-relative mt-150 lg-mt-80">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-xxl-5 col-lg-6 ms-auto d-flex flex-column order-lg-last wow fadeInRight">
+						<div class="title-one">
+							<div class="upper-title text-dark">Who we are</div>
+							<h2>About us.</h2>
+						</div>
+                        <p class="mt-35 lg-mt-10">Trading your digital assets for cash has never been easier. Our platform offers a quick, secure, and convenient way to sell your cryptocurrency or gift cards, with no hidden fees or complicated procedures.</p>
+						<p class="mb-10">At TWJ Hub, we're dedicated to providing the best possible experience for our customers. We understand that selling cryptocurrency, gift cards or paying bills can be complex, so we've designed our platform to be simple, secure, and user-friendly. When you choose TWJ Hub, you become part of a community that values trust, transparency, and exceptional service.</p>
+                        <a href="contact" class="btn-three icon-link mt-15 md-mb-40">
+                            <span>Request a Callback</span>
+                            <img src="images/lazy.svg" data-src="images/icon/icon_09.svg" alt="" class="lazy-img icon ms-1">
+                        </a>
+						<div class="counter-wrapper mt-30 lg-mt-20 pt-5">
+							<div class="row">
+							</div>
+						</div>
+					</div>
+					<div class="col-xxl-7 col-lg-6 wow fadeInLeft">
+						<div class="media-list-item me-auto pe-xxl-5 pe-4 ps-xxl-5 ps-4 pb-35 md-mt-40 d-flex align-items-end">
+							<ul class="style-none">
+								<li>Secured</li><br>
+								<li>Fast</li><br>
+								<li>24/7 Support</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+        
+        
+		<div class="text-feature-one mt-100 lg-mt-60 pb-150 lg-pb-80">
+			<div class="container">
+				<div class="line-wrapper position-relative">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 wow fadeInLeft">
+                            <div class="pt-75 lg-pt-40 pb-45 lg-pb-20" style="padding-right: 50px;">
+                                <div class="ps-4">
+                                    <h4 class="fw-bold mb-20">Our Core Values</h4>
+                                    <p>Our core values are built around trust, transparency, and exceptional customer service. We're passionate about creating a world-class platform for digital asset trading. Our commitment to excellence extends to every aspect of our business, from the security of your funds to the quality of our customer support. We're dedicated to providing a hassle-free and enjoyable experience for all of our users.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 ms-auto wow fadeInRight">
+                            <div class="card-style-three pt-75 lg-pt-40 pb-45 lg-pb-20" style="padding-left: 50px;">
+                                <div class="ps-4">
+                                    <h4 class="fw-bold mb-20">Our Mission</h4>
+                                    <p>Our mission is to exceed your expectations when it comes to gift card trading, crypto trading and utility payments. We're committed to providing exceptional customer service and ensuring a smooth, enjoyable experience for all. Our platform is built on trust, transparency, and the pursuit of maximum value.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+			</div>
+		</div>
+        
+        
+
+<?php
+include "./components/app-cta.php";
+include "./components/footer.php";
+?>
---
+++ components/asset-logos.php
@@ -0,0 +1,40 @@
+        <div class="feedback-section-three position-relative">
+			<div class="container">
+				<div class="position-relative">
+                    <div class="partner-logo-one mt-80">
+                        <div class="partner-slider-one">
+                            <div class="item">
+                                <div class="logo d-flex align-items-center justify-content-center"><img src="images/cards/amazon.svg" alt=""></div>
+                            </div>
+                            <div class="item">
+                                <div class="logo d-flex align-items-center justify-content-center"><img src="images/cards/american_express.svg" alt=""></div>
+                            </div>
+                            <div class="item">
+                                <div class="logo d-flex align-items-center justify-content-center"><img src="images/cards/ebay.svg" alt=""></div>
+                            </div>
+                            <div class="item">
+                                <div class="logo d-flex align-items-center justify-content-center"><img src="images/cards/google_play.svg" alt=""></div>
+                            </div>
+                            <div class="item">
+                                <div class="logo d-flex align-items-center justify-content-center"><img src="images/cards/nike.svg" alt=""></div>
+                            </div>
+                            <div class="item">
+                                <div class="logo d-flex align-items-center justify-content-center"><img src="images/cards/nordstom.svg" alt=""></div>
+                            </div>
+                            <div class="item">
+                                <div class="logo d-flex align-items-center justify-content-center"><img src="images/cards/sephora.svg" alt=""></div>
+                            </div>
+							<div class="item">
+                                <div class="logo d-flex align-items-center justify-content-center"><img src="images/cards/steam.svg" alt=""></div>
+                            </div>
+							<div class="item">
+                                <div class="logo d-flex align-items-center justify-content-center"><img src="images/cards/vanilla.svg" alt=""></div>
+                            </div>
+							<div class="item">
+                                <div class="logo d-flex align-items-center justify-content-center"><img src="images/cards/walmart.svg" alt=""></div>
+                            </div>
+                        </div>
+                    </div>
+				</div>
+			</div>
+		</div>
