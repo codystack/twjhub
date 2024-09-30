0333ae85904260049ab3f743632e367cd9ed4236
codystack <ebuzzadvert@gmail.com>
2024-09-30 05:54:59 +0100

    utilities page added

--- components/header.php
+++ components/header.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta charset="UTF-8">
-	<meta name="keywords" content="Digital marketing agency, Digital marketing company, Digital marketing services">
+	<meta name="keywords" content="bitcoin trading, ethereum trading, ripple trading, litecoin trading, bitcoin cash trading, tether trading, cardano trading, polkadot trading, chainlink trading, solana trading, crypto exchange, cryptocurrency exchange, digital asset exchange, bitcoin exchange, ethereum exchange, altcoin exchange, crypto trading platform, cryptocurrency trading platform, digital asset trading platform, Amazon gift card, iTunes gift card, Google Play gift card, Walmart gift card, Target gift card, Best Buy gift card, Sephora gift card, GameStop gift card, Netflix gift card, Starbucks gift card, gift card exchange">
 	<meta name="description" content="Trading your digital assets for cash has never been easier. Our platform offers a quick, secure, and convenient way to sell your cryptocurrency or gift cards, with no hidden fees or complicated procedures.">
     <meta property="og:site_name" content="TWJ Hub">
     <meta property="og:url" content="https://twjhub.com">
@@ -25,25 +25,29 @@
     <script src="vendor/html5shiv.js"></script>
     <script src="vendor/respond.js"></script>
 	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-WT29FNKK');</script>
+	<script>
+		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-WT29FNKK');
+	</script>
 	<!-- End Google Tag Manager -->
 </head>
 
 <body>
 	<!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WT29FNKK"
-	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<noscript>
+		<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WT29FNKK"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe>
+	</noscript>
 	<!-- End Google Tag Manager (noscript) -->
-	 
+
 	<div class="main-page-wrapper">
 		<div id="preloader">
 			<div id="ctn-preloader" class="ctn-preloader">
 				<div class="icon"><img src="images/favicon.png" alt="" class="m-auto d-block" width="60"></div>
-				<!-- <div class="txt-loading">
+				<div class="txt-loading">
 					<span data-text-preloader="T" class="letters-loading">
 						T
 					</span>
@@ -62,6 +66,6 @@
                     <span data-text-preloader="B" class="letters-loading">
 						B
 					</span>
-				</div> -->
+				</div>
 			</div>
 		</div>
--- utilities.php
+++ utilities.php
@@ -1,6 +1,127 @@
 <?php
 include "./components/header.php";
+include "./components/navbar.php";
 ?>
+
+        <div class="inner-banner-one pt-225 lg-pt-200 md-pt-150 pb-100 md-pb-70 position-relative" style="background-image: url(images/utilities.webp);;background-position: center center;">
+			<div class="container position-relative">
+				<div class="row align-items-center">
+					<div class="col-lg-7">
+						<h1 class="hero-heading d-inline-block text-dark position-relative">Pay bills, with <br>no fuss.<img src="images/shape/shape_33.svg" alt=""></h1>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+        <div class="text-feature-four position-relative pt-150 lg-pt-80">
+			<div class="container">
+				<div class="row">
+					<div class="col order-md-last wow fadeInRight text-center">
+						<p class="quote-text text-dark ps-xl-5"><span class="fw-500 text-decoration-underline">Save time</span> and effort with TWJ Hub. Our all-in-one bill payment platform makes it easy to manage and pay all your essential bills from airtime and data to internet, TV, and electricity.</p>
+					</div>
+				</div>
+			</div>
+			<!-- <img src="images/lazy.svg" data-src="images/shape/shape_13.svg" alt="" class="lazy-img shapes shape_01"> -->
+			<img src="images/lazy.svg" data-src="images/shape/shape_13.svg" alt="" class="lazy-img shapes shape_02">
+		</div>
+        
+
+		<div class="text-feature-five position-relative mt-150 lg-mt-80 pb-50">
+			<div class="container">
+				<div class="bg-wrapper" style="border-radius: 50px;">
+                    <div class="row">
+                        <div class="col-lg-6 ms-auto d-flexflex-column order-lg-last">
+                            <div class="text-wrapper pt-50 pb-60 lg-pb-40 ps-3 ps-xxl-5 pe-3 pe-xxl-5">
+                                <div class="title-one">
+                                    <h2 class="text-dark">Pay your bills with ease.</h2>
+                                </div>
+                                <p class="text-lg text-dark mt-25 mb-35 lg-mb-30">TWJ Hub is your one-stop solution for managing and paying all your essential bills. Simplify your life with TWJ Hub. Easily manage and pay all your bills in one convenient place, from airtime and data to internet, TV, and more.</p>
+                                <ul class="style-none">
+                                    <li>Fast and secured</li><br>
+                                    <li>Seamless payments</li><br>
+                                    <li>No Hidden charges</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 d-flex order-lg-first">
+                            <div class="media-wrapper w-100 d-flex align-items-center justify-content-center position-relative" style="background: url(./images/utility-img.webp) no-repeat center;background-size: cover;">
+                                <img src="images/lazy.svg" data-src="images/assets/utility.svg" alt="" class="lazy-img shapes screen_01">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+			</div>
+		</div>
+
+
+
+        <div class="faq-section-one mt-150 lg-mt-120 md-mt-80 mb-100">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5 wow fadeInLeft">
+						<div class="title-one mb-40 lg-mb-20">
+							<h2 class="text-dark">Frequently Asked Questions</h2>
+						</div>
+						<p class="text-lg mb-40 lg-mb-20">Don’t find the answer? We can help</p>
+						<a href="contact" class="btn-four">Contact us</a>
+					</div>
+
+					<div class="col-lg-7">
+						<div class="accordion accordion-style-one mt-15 md-mt-50" id="accordionOne">
+							<div class="accordion-item">
+								<h2 class="accordion-header">
+									<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+										Is it safe to pay bills through TWJ?
+									</button>
+							  	</h2>
+							  	<div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionOne">
+									<div class="accordion-body">
+										<p>Your security is our top priority. At TWJ Hub, we employ the latest security measures to protect your personal information and transactions. You can trust us to keep your data safe.</p>
+									</div>
+							  	</div>
+							</div>
+							<div class="accordion-item">
+								<h2 class="accordion-header">
+									<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        Can I buy electricity on TWJ Hub?
+									</button>
+								</h2>
+								<div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionOne">
+									<div class="accordion-body">
+										<p>Yes, you can. Experience the convenience of online electricity bill payments with TWJ Hub.</p>
+									</div>
+								</div>
+							</div>
+							<div class="accordion-item">
+								<h2 class="accordion-header">
+									<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                        What bills can I pay on TWJ Hub?
+									</button>
+								</h2>
+								<div id="collapseThree" class="accordion-collapse collapse show" data-bs-parent="#accordionOne">
+									<div class="accordion-body">
+										<p>Pay all your bills with ease on TWJ Hub. From airtime and data to internet, TV, and cable, we've got you covered. We're always adding new billers to ensure you have the most convenient bill payment experience possible.</p>
+									</div>
+								</div>
+							</div>
+							<div class="accordion-item">
+								<h2 class="accordion-header">
+									<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                        How do I pay bills on TWJ Hub?
+									</button>
+								</h2>
+								<div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionOne">
+									<div class="accordion-body">
+										<p>To pay your bills, simply log in to your TWJ Hub account, choose the bill you want to pay, enter your details, and confirm your payment.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
 
 
