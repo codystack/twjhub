a0f5e61eb2fef88c3b7fee3af28816d596de3758
codystack <ebuzzadvert@gmail.com>
2024-10-02 03:39:13 +0100

    AML page added

---
+++ aml.php
@@ -0,0 +1,36 @@
+<?php
+include "./components/header.php";
+include "./components/navbar.php";
+?>
+
+        <div class="inner-banner-one pt-225 lg-pt-200 md-pt-150 pb-100 lg-pb-70 md-pb-30 position-relative" style="background-image: url(images/aml-bg.webp);background-position: center center;">
+			<div class="container position-relative">
+				<div class="row align-items-center">
+					<div class="col-lg-8 m-auto text-center">
+						<div class="tag">Legal</div>
+						<h1 class="hero-heading no-bg text-white">Anti-Money Laundry</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+        <div class="pb-150 lg-pb-80">
+			<div class="container mt-150 lg-mt-80">
+				<div class="col-lg-8 mx-auto">
+					<h3>Introduction</h3>
+				    <p>Money laundering is the attempt to conceal or disguise the nature, location, source, ownership or control of illegally obtained money. Money laundering is most commonly associated with tax avoidance. However, other individuals may attempt to launder money in order to conceal their identity or finance their operations. 'Suspicious activity' is a very difficult concept to define because it can vary from one transaction to another based upon all the circumstances surrounding the transaction or group of transactions. For example, transactions by one customer may be normal based on our knowledge of that customer and their pattern of activity, while similar transactions by another customer may be suspicious. Many factors are involved in determining whether transactions are suspicious including the amount, the nature of the transaction and frequency of deposits/withdrawals from the system. TWJ Hub is committed to fighting money laundering and complying fully with anti-money laundering laws in the Nigeria. We understand that we have responsibilities to help fight the global battle against money laundering and our commitment will supersede all other privacy obligations contained in our policies. Accordingly, TWJ Hub will take all reasonable and appropriate steps to prevent persons engaged in money laundering, fraud, or other financial crimes from utilizing our products and services.</p>
+				    <p class="mb-50">Our AML policies, procedures and internal controls are designed to ensure compliance with all applicable BSA regulations and FINRA rules and will be reviewed and updated on a regular basis to ensure appropriate policies, procedures and internal controls are in place to account for both changes in regulations and changes in our business. Key components of our AML and CTF framework include the following:</p>
+                    <h3>Compliance Officer</h3>
+                    <p class="mb-50">Coordinating and monitoring day-to-day compliance with the relevant legislation, regulations, rules and industry guidance and applicable money laundering laws and regulationsMonitoring transactions to detect unusual suspicious activities Prompt preparation and delivery of all relevant returns to the regulatory bodies in line with SEC and FIU Rules and Regulations Communicating AML/CFT issues to all stakeholders</p>
+                    <h3>Risk Committee</h3>
+                    <p class="mb-50">The appointment of an independent risk committee which reports to our board of directors regularly on all risk and compliance matters using a risk-based approach to the assessment and management of money laundering and terrorist financing risks and Comply with the requirements of the Money Laundering (Prohibition) Act, 2011 (as amended), Terrorism (Prevention) Act, 2011 (as amended) and Terrorism Prevention (Freezing of International Terrorists Funds and other Related Measures) Regulations 2013, including related laws and Regulations.</p>
+                    <h3>Internal Control</h3>
+                    <p class="mb-50">Formulate and implement internal controls and other procedures that will deter criminals from using its facilities for money laundering and terrorist financing and to ensure that its obligations under subsisting laws and Regulations are met.</p>
+                    <h3>Know your Customer</h3>
+                    <p>Establishing and maintaining a risk-based approach to Customer Due Diligence (CDD), including customer identification, verification and KYC procedures. To ensure we meet these standards, our customers are required to provide certain personal details when opening a TWJ Hub account and also generate tokens, OTP and PIN for any withdrawal request. The nature, and extent, of what is required is guided by the customer's deposit and withdrawal limits, account to be used and in some cases, the customer's country of residence. In certain circumstances, TWJ Hub may perform enhanced due diligence procedures for customers presenting a higher risk, such as those transacting large volumes etc. Maintaining appropriate KYC records for the minimum prescribed periods; TWJ Hub shall ensure timely and accurate rendition of all AML/CFT returns as specified in the SEC AML/CFT Rules and Regulations as well as other relevant Regulations/Act/Guidelines/Circulars that may be issued from time to time by various government agencies.We shall exercise due diligence in identifying and reporting a suspicious transaction. Suspicious transactions shall include:Transactions which are structured to avoid reporting and record keeping requirementsAltered or false identification or inconsistent information or any transaction involving criminal activity in TWJ Hub's viewEntity that belongs to a person or organisation considered as terrorist. TWJ Hub shall also provide training on the framework and raising awareness among all relevant employees and every Settlement proceeds are only wired to verified bank accounts registered by us.</p>
+                </div>
+            </div>
+		</div>
+
+<?php include "./components/footer.php"; ?>
